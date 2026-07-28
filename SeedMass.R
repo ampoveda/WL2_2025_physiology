423ca636f8fa36d86bc2231a5be1defcd8891551
Angelica Poveda <ampoveda@ucdavis.edu>
2026-07-28 12:20:46 -0700

    Notes for seed mass for blog post

---
+++ SeedMass.R
@@ -0,0 +1,4 @@
+#load library(tidyverse)
+# Read in seed mass data. read.csv()
+# Make histogram plot seedmass |< ggplot(aes(Mass)) + geom_histogram()
+#ggsave("figures/SeedMass_Histogram.png", units="in", width=5,height=5)
